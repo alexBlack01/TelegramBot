d46e2d91f5d83e10fa958aaf73c5b7415adc673e
alexBlack01 <sashatuzikov2001@mail.ru>
2021-03-16 22:10:54 +0700

    Template project

---
+++ bot.py
@@ -0,0 +1,59 @@
+import telebot
+from telebot import types
+
+bot = telebot.TeleBot('1656161614:AAEpFdJfVTZAyoW4xdNSLwkCSHkMQvansgY')
+
+name = ''
+surname = ''
+age = 0
+
+
+@bot.message_handler(content_types=['text', 'picture'])
+def start(message):
+    if message.text == '/start':
+        bot.send_message(message.from_user.id, "Как тебя зовут?")
+        bot.register_next_step_handler(message, get_name)  # следующий шаг – функция get_name
+    else:
+        bot.send_message(message.from_user.id, 'Напиши /reg')
+
+
+def get_name(message):  # получаем фамилию
+    global name
+    name = message.text
+    bot.send_message(message.from_user.id, 'Какая у тебя фамилия?')
+    bot.register_next_step_handler(message, get_surname)
+
+
+def get_surname(message):
+    global surname
+    surname = message.text
+    bot.send_message(message.from_user.id, 'Сколько тебе лет?')
+    bot.register_next_step_handler(message, get_age)
+
+
+def get_age(message):
+    global age
+    while age == 0:  # проверяем что возраст изменился
+        try:
+            age = int(message.text)  # проверяем, что возраст введен корректно
+        except Exception:
+            bot.send_message(message.from_user.id, 'Цифрами, пожалуйста')
+    keyboard = types.InlineKeyboardMarkup()  # наша клавиатура
+    key_yes = types.InlineKeyboardButton(text='Да', callback_data='yes')  # кнопка «Да»
+    keyboard.add(key_yes)  # добавляем кнопку в клавиатуру
+    key_no = types.InlineKeyboardButton(text='Нет', callback_data='no')
+    keyboard.add(key_no)
+    question = 'Тебе ' + str(age) + ' лет, тебя зовут ' + name + ' ' + surname + '?'
+    bot.send_message(message.from_user.id, text=question, reply_markup=keyboard)
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_worker(call):
+    if call.data == "yes":  # call.data это callback_data, которую мы указали при объявлении кнопки
+        print("Data save")  # код сохранения данных, или их обработки
+        bot.send_message(call.message.chat.id, 'Запомню : )');
+    elif call.data == "no":
+        print("Data not save")  # переспрашиваем
+
+
+bot.polling(none_stop=True, interval=0)
