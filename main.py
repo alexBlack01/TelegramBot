64287ce7f705d40156b609d9aceb3d3151982445
Alex <sashatuzikov2001@mail.ru>
2021-04-08 00:43:18 +0700

    Add function state

--- main.py
+++ main.py
@@ -54,12 +54,4 @@ def check_resolution(message):
 
 bot.polling(none_stop=True)
 
-#@bot.callback_query_handler(func=lambda call: True)
-#def callback_worker(call):
-#    if call.data == "yes":  # call.data это callback_data, которую мы указали при объявлении кнопки
-#        print("Data save")  # код сохранения данных, или их обработки
-#        bot.send_message(call.message.chat.id, 'Запомню : )');
-#    elif call.data == "no":
-#        print("Data not save")  # переспрашиваем
-
 
--- registration.py
+++ registration.py
@@ -13,7 +13,9 @@ surname = ''
 age = 0
 sex = ''
 city = ''
+photo = ''
 
+@bot.message_handler(func=lambda message: db_users.get_current_state(message.from_user.id) == config.S_GET_NAME)
 def get_name(message):
     global name
     name = message.text
@@ -22,6 +24,7 @@ def get_name(message):
     bot.send_message(message.from_user.id, 'Сколько тебе лет?')
     bot.register_next_step_handler(message, get_age)
 
+@bot.message_handler(func=lambda message: db_users.get_current_state(message.from_user.id) == config.S_GET_AGE)
 def get_age(message):
     global age
     if not message.text.isdigit():
@@ -30,26 +33,43 @@ def get_age(message):
 
     age = int(message.text)
     user.age = age
-    bot.send_message(message.from_user.id, 'А какой у тебя пол?')
     bot.register_next_step_handler(message, get_sex)
 
+@bot.message_handler(func=lambda message: db_users.get_current_state(message.from_user.id) == config.S_GET_SEX)
 def get_sex(message):
     global sex
-    keyboard = types.InlineKeyboardMarkup()  # наша клавиатура
-    key_male = types.InlineKeyboardButton(text='М', callback_data='male')  # кнопка «Да»
-    keyboard.add(key_male)  # добавляем кнопку в клавиатуру
-    key_female = types.InlineKeyboardButton(text='Ж', callback_data='female')
-    keyboard.add(key_female)
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    key_man = types.KeyboardButton(text='М')
+    key_woman = types.KeyboardButton(text='Ж')
+    keyboard.row(key_man, key_woman)
 
-    bot.send_message(message.from_user.id, 'А какой у тебя пол?')
-    bot.register_next_step_handler(message, get_sex)
-    question = 'Тебе ' + str(age) + ' лет, тебя зовут ' + name + ' ' + '?'
-    bot.send_message(message.from_user.id, text=question, reply_markup=keyboard)
+    bot.send_message(message.from_user.id, "А какой у тебя пол?", reply_markup=keyboard)
 
+    sex = message.text
+    user.sex = sex
+    bot.send_message(message.from_user.id, 'А в каком городе ты находишься?')
+    bot.register_next_step_handler(message, get_city)
+
+@bot.message_handler(func=lambda message: db_users.get_current_state(message.from_user.id) == config.S_GET_CITY)
 def get_city(message):
     global city
     city = message.text
     user.city = city
 
-def add_user_in_db():
+    bot.register_next_step_handler(message, add_user_in_db)
+
+@bot.message_handler(content_types=['document'], func=lambda message: db_users.get_current_state(message.from_user.id) == config.S_GET_PHOTO)
+def get_photo(message):
+
+    global photo
+    file_info = bot.get_file(message.document.file_id)
+    downloaded_file = bot.download_file(file_info.file_path)
+
+    src = 'C:/Python/Project/tg_bot/files/received/' + message.document.file_name;
+    with open(src, 'wb') as new_file:
+        new_file.write(downloaded_file)
+
+
+def add_user_in_db(message):
     db_users.check_and_add_user(user)
+    bot.send_message(message.from_user.id, 'Спасибо за регистрацию!')
