9cdc1b6af957857ff0adeab75525b2e168a96dde
Alex <sashatuzikov2001@mail.ru>
2021-04-08 15:44:05 +0700

    Add function save_user_form in db

--- db_users.py
+++ db_users.py
@@ -1,5 +1,6 @@
 import pymongo
 from pymongo import MongoClient
+from datetime import datetime
 from config import MONGODB_LINK
 from config import MONGO_DB
 import config
@@ -7,22 +8,36 @@ import user
 
 db = MongoClient(MONGODB_LINK)[MONGO_DB]
 
-def check_and_add_user(cur_user: user.User()):
-    if db.users.find_one({'user_id': user.id}) is None:
+def check_and_add_user(message):
+    user = db.users.find_one({'user_id': message.from_user.id.id})
+    if user is None:
         new_user = {
-            'user_id': cur_user.id,
-            'name': cur_user.name,
-            'age': cur_user.age,
-            'sex': cur_user.sex,
-            'city': cur_user.city,
-            'photo': cur_user.photo,
-            'state': cur_user.state
+            'user_id': message.from_user.id,
+            'first_name': message.from_user.first_name,
+            'last_name': message.from_user.last_name,
+            'date': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+            'state': 'старт'
         }
         db.users.insert_one(new_user)
     return
 
+def save_user_form(cur_user: user.User()):
+    db.users.update_one(
+        {'user_id': cur_user.id},
+        {'$set': {'form': {'name': cur_user.name,
+                            'age': cur_user.age,
+                            'sex': cur_user.sex,
+                            'city': cur_user.city,
+                            'photo': cur_user.photo,
+                            'state': cur_user.state
+                             }
+                  }
+         }
+    )
+    return user
+
 def get_current_state(user_id):
-    cur_user = db.users.find_one({'user_id':user_id})
+    cur_user = db.users.find_one({'user_id': user_id})
     return cur_user['state']
 
 def set_state(user_id, state_value):
