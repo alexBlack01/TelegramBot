1d1c93c280e41d3fffbad1eb0d170ea938ee236b
alexBlack01 <sashatuzikov2001@mail.ru>
2021-05-08 22:57:29 +0700

    Add a function to interact with the form

--- search_user.py
+++ search_user.py
@@ -1,8 +1,15 @@
 import random
+import config
 import db_users
 
-from aiogram import types
+from aiogram import types, Bot, Dispatcher
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher.filters.state import State, StatesGroup
+
+from main import base_menu
+
+bot = Bot(token=config.TOKEN, parse_mode=types.ParseMode.HTML)
+dp = Dispatcher(bot, storage=MemoryStorage())
 
 
 class StageSearch(StatesGroup):
@@ -13,15 +20,32 @@ async def regular_search(message: types.Message):
     users = db_users.get_all_users()
     user = random.choice(users)
 
-    await message.answer('Меня зовут <b>Meet&Greet</b>.\n\n'
-                         'Я открою тебе мир новых знакомств.\n'
-                         'Но чтобы начать, мне нужно узнать у тебя некоторое количество данных.\n'
-                         'Однако я не могу обрабатывать твои данные без твоего согласия.')
+    caption = f'{user.name}, {user.age}, {user.city}'
+    await bot.send_photo(message.from_user.id, photo=user.photo, caption=caption)
 
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.row('1', '2', '3', '4')
 
-    await bot.send_photo(message.from_user.id, CAT_BIG_EYES,
-                         caption=emojize(caption),
-                         reply_to_message_id=message.message_id)
+    await message.answer('1 - Лайк\n'
+                         '2 - Дизлайк\n'
+                         '3 - Сон\n'
+                         '4 - Изменить параметры', reply_markup=keyboard)
+
     await StageSearch.waiting_for_regular_search.set()
 
-async def regular_search_choose():
+
+async def regular_search_choose(message: types.Message):
+    if message.text == '1':
+        await message.answer('Реквест отправлен!')
+        await regular_search(message)
+    if message.text == '2':
+        await message.answer('Анкета пропущена!')
+        await regular_search(message)
+    if message.text == '3':
+        await message.answer('Сон!')
+        await regular_search(message)
+    if message.text == '4':
+        await base_menu(message)
+    else:
+        await message.answer('Выбери что-то из предложенного!')
+        return
